796801d284611e1608a9d3c151a43599a74d9247
Medrano Brian <67834746+DallasSpawn@users.noreply.github.com>
2022-09-22 09:35:44 -0500

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// Se asegura de que el navegador usado tiene soporte para service worker
-if (navigator.serviceWorker) {
-    console.log("Es compatible sin escrúpulos");
- 
-    navigator.serviceWorker.register("./sw.js");
-}
